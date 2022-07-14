b2d591b1c9e3861cf54aa63f703c0cb92bd7fa0a
Evgeniy Biryuk <107886633+Kvilion@users.noreply.github.com>
2022-07-14 18:33:01 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(function(){
+ $('.header-slider').slick({
+  infinite: true,
+  slidesToShow: 1,
+   arrows: false,
+   vertical: true,
+   dots: true,
+   dotsClass: 'header-dots',
+   autoplay: 2000
+  
+});
+});
